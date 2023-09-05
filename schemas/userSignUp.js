44dc013a333d2401135eb0dd8c8f749de1afeb4f
Juan Rivera <indiojuan2012.jr@gmail.com>
2023-09-05 20:36:15 -0300

    modifique register

--- schemas/userSignUp.js
+++ schemas/userSignUp.js
@@ -5,11 +5,11 @@ const schema = Joi.object({
     mail: Joi
         .string()
         .required()
-        .min(8)
-        .email({ minDomainSegments: 2 })
+        .min(6)
+
         .messages(
             {
-                'string.min': 'El correo debe tener al menos 8 caracteres',
+                'string.min': 'El correo debe tener al menos 6 caracteres',
                 'string.empty': 'El correo no puede estar vacío',
                 'any.required': 'Se requiere un correo',
                 'string.email': 'Es necesario un correo válido'
@@ -18,11 +18,11 @@ const schema = Joi.object({
     password: Joi
         .string()
         .required()
-        .min(8)
+        .min(6)
         .max(50)
         .messages(
             {
-                'string.min': 'La contraseña debe tener al menos 8 caracteres',
+                'string.min': 'La contraseña debe tener al menos 6 caracteres',
                 'string.max': 'La contraseña no puede exceder los 50 caracteres',
                 'string.empty': 'La contraseña no puede estar vacía',
                 'any.required': 'Se requiere una contraseña',
